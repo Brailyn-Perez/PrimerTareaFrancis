892c47cfbea704ba6f5c2708913660368fb7554c
Broly 12 <brailynperezbatista@gmail.com>
2025-01-18 20:49:07 -0400

    ok

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public abstract class Persona
+{
+    public string Nombre { get; set; }
+}
+
+public class Estudiante : Persona
+{
+    public string NumeroUnico { get; set; }
+}
+
+public class Profesor : Persona
+{
+    public List<Curso> Cursos { get; set; } = new List<Curso>();
+}
+
+public class Curso
+{
+    public string Nombre { get; set; }
+    public int RecuentoClases { get; set; }
+    public int RecuentoEjercicios { get; set; }
+}
+
+public class Clase
+{
+    public string Identificador { get; set; }
+    public List<Estudiante> Estudiantes { get; set; } = new List<Estudiante>();
+    public List<Profesor> Profesores { get; set; } = new List<Profesor>();
+}
